a32df26f1246c05db97bf15b8fbd0a6072b39319
Adair M <adairmtz13@hotmail.com>
2023-06-26 12:11:30 -0600

    new version

--- app/src/main/java/com/example/registrargasto/viewTutorial/TutorialPresupuestoActivity.java
+++ app/src/main/java/com/example/registrargasto/viewTutorial/TutorialPresupuestoActivity.java
@@ -2,6 +2,7 @@ package com.example.registrargasto.viewTutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,15 +14,17 @@ import com.example.registrargasto.R;
 public class TutorialPresupuestoActivity extends AppCompatActivity {
     private ImageView atras;
     private TextView skip;
+    private ImageView iniciar;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial_presupuesto);
         atras = findViewById(R.id.btn_atras);
         skip = findViewById(R.id.btn_skip);
-
-
+        iniciar = findViewById(R.id.btn_iniciar);
+        btnIniciar();
         btnAtras();
         btnSkip();
     }
@@ -38,6 +41,16 @@ public class TutorialPresupuestoActivity extends AppCompatActivity {
 
     public void btnSkip(){
         skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void btnIniciar(){
+        iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
